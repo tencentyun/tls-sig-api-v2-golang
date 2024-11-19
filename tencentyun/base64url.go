a13dfd1acc578a480fc8eeecc092c0124c98cb7b
okhowang(王沛文) <okhowang@tencent.com>
2024-11-19 14:20:43 +0800

    perf: optimize memory allocation
    before:
    BenchmarkGenSig
    BenchmarkGenSig-16               	  191563	      6149 ns/op	    4400 B/op	      41 allocs/op
    BenchmarkGenUserSigWithBuf
    BenchmarkGenUserSigWithBuf-16    	  180298	      6598 ns/op	    4357 B/op	      45 allocs/op
    
    after:
    BenchmarkGenSig
    BenchmarkGenSig-16               	  394201	      3111 ns/op	    2131 B/op	      19 allocs/op
    BenchmarkGenUserSigWithBuf
    BenchmarkGenUserSigWithBuf-16    	  372675	      3170 ns/op	    1866 B/op	      22 allocs/op

--- tencentyun/base64url.go
+++ tencentyun/base64url.go
@@ -5,13 +5,7 @@ import (
 	"strings"
 )
 
-func base64urlEncode(data []byte) string {
-	str := base64.StdEncoding.EncodeToString(data)
-	str = strings.Replace(str, "+", "*", -1)
-	str = strings.Replace(str, "/", "-", -1)
-	str = strings.Replace(str, "=", "_", -1)
-	return str
-}
+var base64url = base64.NewEncoding("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789*-").WithPadding('_')
 
 func base64urlDecode(str string) ([]byte, error) {
 	str = strings.Replace(str, "_", "=", -1)
